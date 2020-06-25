76f5627d15e5f3f6db2967adb742df1c36ca1dc6
Eric Rees <erees@protonmaill.com>
2020-06-25 15:41:21 -0400

    Changing to front-page and starting webkit install.

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<?php get_header() ?>
+
+<div class="container"> 
+  <div class="d-flex justify-content-center">
+    <div class="main-content">
+      <ul>
+        <li><a href="<?php echo site_url('/development') ?>">development</a></li>
+        <li><a href="<?php echo site_url('/marketing') ?>">marketing</a></li>
+        <li><a href="<?php echo site_url('/about-me') ?>">about me</a></li>
+        <li><a href="<?php echo site_url('/contact') ?>">contact</a></li>
+        <li><a href="<?php echo site_url('/experimental') ?>">experimental</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+<?php get_footer() ?>
--- index.php
+++ index.php
@@ -3,13 +3,7 @@
 <div class="container"> 
   <div class="d-flex justify-content-center">
     <div class="main-content">
-      <ul>
-        <li><a href="#">development</a></li>
-        <li><a href="#">marketing</a></li>
-        <li><a href="#">about me</a></li>
-        <li><a href="#">contact</a></li>
-        <li><a href="#">experimental</a></li>
-      </ul>
+      <p>Blog</p>
     </div>
   </div>
 </div>
