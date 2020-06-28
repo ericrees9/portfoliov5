e1e3a02988d424839be0c19fe31726ddbc24f6c6
Eric Rees <erees@protonmail.com>
2020-06-27 22:15:33 -0400

    Switching over to new file structure and SCSS.

---
+++ js/src/app.js
@@ -0,0 +1,2 @@
+import '../../css/src/app.scss';
+import 'bootstrap';
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,49 @@
 const webpack = require('webpack');
 const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const config = {
   mode: 'development',
-  entry: './src/js/scripts.js',
+  entry: ['./js/src/app.js', './css/src/app.scss'],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname),
+    filename: './js/build/app.min.js'
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+          presets: ['babel-preset-env']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
         use: [
-          'style-loader',
-          'css-loader'
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          'css-loader',
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: 'postcss-loader',
+            options: {
+              plugins: function () {
+                return [
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          // Compiles Sass to CSS
+          'sass-loader',
         ]
       },
       {
@@ -32,6 +61,25 @@ const config = {
         test: /\.svg$/,
         use: 'file-loader'
       }
+    ]
+  },
+  plugins: [
+    // extract css into dedicated file
+    new MiniCssExtractPlugin({
+      filename: './css/build/style.min.css'
+    }), 
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['./js/build/*','./css/build/*']
+    })
+  ],
+  optimization: {
+    minimizer: [
+      // enable the js minification plugin
+      new UglifyJSPlugin({
+        cache: true,
+        parallel: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
     ]
   }
 };
