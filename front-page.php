6dab5b8e48db528a13500b23d6c9c221b6568e1c
Eric Rees <erees@protonmail.com>
2020-06-27 22:56:49 -0400

    SCSS module file buildout begins.

--- front-page.php
+++ front-page.php
@@ -1,15 +1,23 @@
+<?php
+/*
+ Template Name: Front Page Template
+ */
+?>
+
 <?php get_header() ?>
 
-<div class="container"> 
-  <div class="d-flex justify-content-center">
-    <div class="main-content">
-      <ul>
-        <li><a href="<?php echo site_url('/development') ?>">development</a></li>
-        <li><a href="<?php echo site_url('/marketing') ?>">marketing</a></li>
-        <li><a href="<?php echo site_url('/about-me') ?>">about me</a></li>
-        <li><a href="<?php echo site_url('/contact') ?>">contact</a></li>
-        <li><a href="<?php echo site_url('/experimental') ?>">experimental</a></li>
-      </ul>
+<div class="front-page-wrapper">
+  <div class="container"> 
+    <div class="d-flex justify-content-center">
+      <div class="main-content">
+        <ul>
+          <li><a href="<?php echo site_url('/development') ?>">development</a></li>
+          <li><a href="<?php echo site_url('/marketing') ?>">marketing</a></li>
+          <li><a href="<?php echo site_url('/about-me') ?>">about me</a></li>
+          <li><a href="<?php echo site_url('/contact') ?>">contact</a></li>
+          <li><a href="<?php echo site_url('/experimental') ?>">experimental</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </div>
